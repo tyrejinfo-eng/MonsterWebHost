3c93d670247fee592e95f5f4c1bcd907076b6135
Ty Rejinfo <ty.rejinfo@gmail.com>
2026-07-09 21:30:22 +0200

    Delete MonsterWebHost directory

--- MonsterWebHost/Models/WebsiteState.cs
+++
@@ -1,9 +0,0 @@
-namespace MonsterWebHost.Models;
-
-public sealed class WebsiteState
-{
-    public bool IsHostRunning { get; init; }
-    public bool IsOnline { get; init; }
-    public string LocalUrl { get; init; } = string.Empty;
-    public string CloudflaredStatus { get; init; } = "Cloudflared stopped";
-}
